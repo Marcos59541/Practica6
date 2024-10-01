21903cf420cc758a00fa97c4c89479d68f5e268c
Marcos59541 <marcospime662@gmail.com>
2024-10-01 17:06:36 -0600

    Agregar archivos de proyecto.

---
+++ Practica6/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Practica6
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort("COM6", 9600);  
+            serialPort.Open();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine("b");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine("a");
+        }
+    }
+}
